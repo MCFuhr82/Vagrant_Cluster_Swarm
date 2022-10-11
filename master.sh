079513acab446840c10088a370c61e3f1e6da774
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2022-10-11 17:22:41 -0300

    Revert "Docker Swar with Vagrant"
    This reverts commit 40ec961328d954654ae567e771895b064b6ec863.

--- master.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Iniciando o cluster...."
-
-sudo docker swarm init --advertise-addr=10.10.0.111
-sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
--- worker.sh
+++
@@ -1,1 +0,0 @@
-    docker swarm join --token SWMTKN-1-11j5bnxe20b4297uonbi0rn9b0sfs4t55c2tw4xsls3wkhq0w1-0bzeibtiultoxvx71lxzldbsp 10.10.0.111:2377
